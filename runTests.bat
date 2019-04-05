73177379be47b895d3550b3c6888a093a51beca0
brandonflude <brandonflude@192.168.0.18>
2019-04-05 13:21:20 +0100

    Updated Windows run tool

--- runTests.bat
+++ runTests.bat
@@ -1,5 +1,14 @@
+echo "=== If Statements ==="
 java -classpath ./bin SimpleScript < tests/test01.ss
+
+echo "=== Maths With Different Types ==="
 java -classpath ./bin SimpleScript < tests/test02.ss
+
+echo "=== Fibonacci Sequence ==="
 java -classpath ./bin SimpleScript < tests/test03.ss
+
+echo "=== Sorting Arrays ==="
 java -classpath ./bin SimpleScript < tests/test04.ss
+
+echo "=== Deprecated Functions ==="
 java -classpath ./bin SimpleScript < tests/test05.ss
